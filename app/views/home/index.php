cbf23377221f611ceb91d0178905eb3a1cdf3566
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-05-07 00:53:56 +0530

    Add files via upload

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,7 @@
-<?php $this -> setSiteTitle('Home'); ?>
 
-<?php $this-> start('head'); ?>
-    <meta content="Home Content"/>
-    <link rel="stylesheet" href="<?=PROOT?>app/views/home/home.css" media="screen" title="no title" charset = "utf-8">
-<?php $this ->end(); ?>
+<?php $this->start('body'); ?>
+<h1 class="text-center red">This is my MVC Framework project </h1>
 
-<?php $this-> start('body'); ?>
-<h1 class="text-center text-primary "> Welcome to PCCS MVC Framework </h1>
 
-<?php $this ->end(); ?>
+<?php $this->end(); ?>
+
