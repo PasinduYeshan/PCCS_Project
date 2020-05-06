d47859b147c1676278103d503757408108a57b01
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-05-07 00:54:34 +0530

    Add files via upload

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,27 +1,22 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title><?=$this->_siteTitle ?></title>
-    <!---------------- Including css files ---------------->
-    <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css" media="screen" title="no title" charset = "utf-8">
-    <link rel="stylesheet" href="<?=PROOT?>css/custom.css" media="screen" title="no title" charset = "utf-8">
-    <!---------------- Including js files ---------------->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?= $this->siteTitle(); ?></title>
+    <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css" media="screen" title = "no title" charset = "utf-8">
+    <link rel="stylesheet" href="<?=PROOT?>css/custom.css" media="screen" title = "no title" charset = "utf-8">
     <script src = "<?=PROOT?>js/jQuery-2.2.4.min.js"></script>
     <script src = "<?=PROOT?>js/bootstrap.min.js"></script>
-
-    <!----------------- Connecting to the head in app/view ---------------->
     <?= $this->content('head'); ?>
-    
-  </head>
-  <body>
-    <!----------------- Connecting to the body in app/view ---------------->
-    <?=$this->content('body'); ?>
 
+</head>
+    <body>
+<?php include 'main_menu.php' ?>
+        <div class="container-fluid" style="min-height:calc(100% - 125px);">
+        <?= $this->content('body'); ?>
+        </div>
 
-    
-  </body>
+    </body>
 </html>
