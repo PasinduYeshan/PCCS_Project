6c6fa4e0f9a98d08190bb0fa757cbf7690c4924b
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-05-07 00:49:35 +0530

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,15 +1,23 @@
 <?php
 
-    define('DEBUG' ,true);
+define('DEBUG',true);
 
-    define('DB_NAME' ,'mvc');
-    define('DB_USER' ,'root');
-    define('DB_PASSWORD' ,'');
-    define('DB_HOST','127.0.0.1');
-    
+define('DB_NAME','mvc');                                                  //database name
+define('DB_USER','root');                                                 //database user
+define('DB_PASSWORD','');                                                 //database password
+define('DB_HOST','127.0.0.1');                                            //database host *** use IP address to avoid DNS lookup
 
-    define('DEFAULT_CONTROLLER','Home'); //If there's no controller in the URL this will be the default controller
-    define('DEFAULT_LAYOUT' ,'default'); //If no layout is set use this layout
-    
-    define('PROOT','/PCCS/'); //set this to '/' for a live server
-    define('SITE_TITLE','PCCS Framework'); //This is set to site title if nothing is set
+
+define('DEFAULT_CONTROLLER' , 'Home');                                    // default controller if there isn't one defined in the url
+define('DEFAULT_LAYOUT','default');                                       //if no layout is set in the controller use this layout
+
+define('SITE_TITLE','MVC Framework Project');                             //this will be used if no site title is set
+define('MENU_BRAND','MyMVC');                                             //this is the brand text in the menu
+
+define('PROOT', '/MVC59/');                                          // set this to '/' for a live server.
+
+define('CURRENT_USER_SESSION_NAME','kdlasfjaKjdfjASLKFDFdgRIG');          //session name for logged in user
+define('REMEMBER_ME_COOKIE_NAME','ASLKFSFJO32424AFS8F9AFAF98FHI');       //cookie name for logged in user remember me
+define('REMEMBER_ME_COOKIE_EXPIRY',2592000);                               //time in seconds for remember me cookie to live (30 days)
+
+define('ACCESS_RESTRICTED', 'Restricted');                                   //controller name for the restricted redirect
