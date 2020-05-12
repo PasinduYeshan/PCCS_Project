1f5572070d3810db3671078a83f2081b56c618d7
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-05-12 10:21:01 +0530

    licence model added

---
+++ app/models/Licence.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Licence extends Model{
+
+    public $licence_no,$id_no,$full_name,$address,$dob,$blood_group,$competent_to_drive,$issue_date,$expiry_date;
+
+    public function __construct()
+    {
+        $table = 'licence';
+        parent::__construct($table);
+    }
+
+
+    public function findById($id_no,$params = []){
+        $conditions = [
+            'conditions' => 'id_no = ?',
+            'bind' => [$id_no]
+        ];
+        $conditions = array_merge($conditions,$params);
+        return $this->find($conditions);
+    }
+
+
+}
