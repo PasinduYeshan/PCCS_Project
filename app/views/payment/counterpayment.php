7a07e87e6842df4bf3a36d1db7daa6e8d79c22a6
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-05-15 21:41:39 +0530

    payment view

---
+++ app/views/payment/counterpayment.php
@@ -0,0 +1,4 @@
+<?php $this->setSiteTitle('Counter Payment'); ?>
+<?php $this->start('body'); ?>
+<h1 class="text-center red">Pay over the counter.</h1>
+<?php $this->end(); ?>
--- app/views/payment/details.php
+++ app/views/payment/details.php
@@ -1,1 +1,43 @@
+<?php $this->setSiteTitle('Fine Sheet Search'); ?>
+<?php $this->start('body'); ?>
+    <div class="col-md-8 col-md-offset-2 well">
+        <h2 class="text-center">Fine Sheet Search</h2>
+        <hr>
+        <form class="form" action="" method="post">
+            <?= inputBlock('text','Enter ID no','id_no','',['class'=>'form-control'],['class'=>'form-group col-md-6']); ?>
+            <div class="col-md-12 text-right">
+                <a href="<?=PROOT?>home" class="btn btn-default">Cancel</a>
+                <?= submitTag('Search',['class'=>'btn btn-primary', 'name'=>'search']) ;?>
+            </div>
 
+        </form>
+    </div>
+
+    <div>
+        <table class="table table-striped table-condensed table-bordered table-hover">
+            <thead>
+            <th>Finesheet No </th>
+            <th>Fine Amount</th>
+            <th></th>
+            </thead>
+            <tbody>
+            <?php if ($_POST){ foreach ($this->finesheets as $finesheet):?>
+                <tr>
+
+                    <td><?= $finesheet->sheet_no; ?></td>
+                    <td><?=  'Rs. '.$finesheet->fine; ?></td>
+                    <td>
+                        <a href="<?=PROOT?><?=$this->controller?>/counterpayment/<?=$finesheet->sheet_no?>" id="pay" class="btn btn-info btn-sm" onclick="if(!confirm('Are you sure you want to mark this payment?')){return false;}">
+
+                            <i class="glyphicon glyphicon-usd"></i>Mark Payment
+                        </a>
+
+                    </td>
+                </tr>
+            <?php endforeach;}?>
+            </tbody>
+
+        </table>
+
+    </div>
+<?php $this->end(); ?>
