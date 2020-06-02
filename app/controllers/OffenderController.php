a76a5f39c9bad4145fc55e4f888c97bb502328e6
PasinduYeshan <61885844+PasinduYeshan@users.noreply.github.com>
2020-06-02 09:42:09 +0530

    .

--- app/controllers/OffenderController.php
+++ app/controllers/OffenderController.php
@@ -2,28 +2,29 @@
 
 
 class OffenderController extends UserController{
-    private $finesheetC;
-    private $licenceC;
+    private $name, $id, $licenceNumber, $tp_no, $address, $nearest_policeStation;
+    private $myFinesheet;
+    private $myLicence;
 
     public function __construct($controller, $action)
     {
         parent::__construct($controller, $action);
         $this->view->setLayout('default');
-        $this->finesheetC = new FinesheetController($controller,$action);
-        $this->licenceC = new LicenceController($controller,$action);
+        $this->myFinesheet = new FinesheetController($controller,$action);
+        $this->myLicence = new LicenceController($controller,$action);
     }
 
 
     public function myfinesAction(){
-        $this->finesheetC->myfinesAction();
+        $this->myFinesheet->myfinesAction();
     }
 
     public function viewAction($sheet_no){
-        $this->finesheetC->viewAction($sheet_no);
+        $this->myFinesheet->viewAction($sheet_no);
     }
 
     public function mylicenceAction(){
-        $this->licenceC->mylicenceAction();
+        $this->myLicence->mylicenceAction();
     }
 
 }
