e1dbc69996cdcef167510f0675a4ec850a345dba
cmgchess <61736812+cmgchess@users.noreply.github.com>
2020-07-22 13:15:08 +0530

    Add files via upload

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,34 +3,25 @@
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
-<!-- <img src = "<?=PROOT?>css/images/homePolice.jpg" class="img-fluid" id= "policeImg"> -->
-<div class="conatiner" id="topic">
-    <h1 class="text-center" style="color :aliceblue" >PCCS Online Traffic Police System</h1>
-</div>
-<div class="container">
-    <h1><br>
-    <br><br><br><br><br>
-    </h1>
-</div>
-<footer>
-    <div class = "container-fluid foo" id = "service-part">
-        <div class = "row text-center">
-            <div class = "col-12">
-                <h4>Contact Us</h4>
-                <hr class = "light">
-                <div class="col-sm-3">
-                    <p>0777453588</p>
-                    <p>pasinduyeshann@gmail.com</p>
-                </div>
-                
-            </div>
-
-            <div class="col-12">
-                <hr class="light-100">
-                <h5>&copy; Have a nice day!</h5>
-            </div>
-        </div>
+<?php if (!currentUser()):?>
+    <img src = "<?=PROOT?>css/images/homePolice.jpg" class="img-fluid" id= "policeImg">
+    <div class="conatiner" id="topic">
+        <h1 class="text-center">PCCS Online Traffic Police System</h1>
     </div>
-</footer>
+<?php elseif  (currentUser()->acl=='["TrafficOfficer"]'): ?>
+    <img src = "<?=PROOT?>css/images/aa.png" class="img-fluid" id= "policeImg">
+    <div class="conatiner" id="topic">
+        <h1 class="text-center">PCCS Online Traffic Police System</h1>
+    </div>
+<?php else : ?>
+    <img src = "<?=PROOT?>css/images/homePolice.jpg" class="img-fluid" id= "policeImg">
+    <div class="conatiner" id="topic">
+        <h1 class="text-center">PCCS Online Traffic Police System. You are Offender</h1>
+
+    </div>
+
+<?php endif; ?>
+
+
 <?php $this->end(); ?>
 
