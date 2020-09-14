47da70a7bbe5fee259866ac63553a3d94f313f9a
PasinduYeshan <61885844+PasinduYeshan@users.noreply.github.com>
2020-09-14 19:08:44 +0530

    .

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -48,82 +48,37 @@ class ReportController extends Controller{
         }
         return $offenceWithCounts;
 	 }
-     /*public function branchreportAction(){
-         if ($_POST){
-           //This is how we can get the aray for the branch report
-            //$branch_id = 1;
+     
+    public function branchreportAction(){
+    if(currentUser()->acl=='["BranchOIC"]'){
+            $oic = new OIC();
+            $branch = new BranchDomain($oic->findById(currentUser()->id)[0]->branch);
+            if ($_POST){
             $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
-                if($_POST['branch_id']<=15 && $_POST['branch_id']>=1){
-                    $branch = new BranchDomain($_POST['branch_id']); //BranchGroup
-                    $branch->accept($branchReport);
-                    $reportArray = $branchReport->getReportArray();
-                    $offenceCountsAll=array();
-                    $vehicleNames=array('SLTB','Private','Lorry','Container','Car','Dual','Motorcycle','Three-Wheeler','Light bus','Light Lorry','Tractor','Hand tractor','Bicycle','Pedestrian');
-                    for ($x = 1; $x <= 33; $x++) {
-                        $offenceCountEach=array();
-                        foreach ($vehicleNames as $vehicleType) {
-                            $offenceCountEach[$vehicleType]=$reportArray[$vehicleType][$x];
-                        }
-                        $offenceCountsAll[$x]=$offenceCountEach;
-                    }
-                    $offence=new Offence();
-                    $offenceNames=array();
-                    $offenceWithCounts=array();
-                    for($i=1;$i<34;$i++){
-                        $offences=$offence->findById($i);
-                        foreach($offences as $of){
-                            array_push($offenceNames,"$of->offence_name");
-	    	            }
-                        array_push($offenceWithCounts,"$of->offence_name",$offenceCountsAll[$i]);//edited
-                    	}
-                    	$this->branchPdfReport($offenceWithCounts);
-		    } 
-         	}
-            $this->view->render('report/branchreport');
-     }*/
-     public function branchreportAction(){
-        if(currentUser()->acl=='["BranchOIC"]'){
-             $oic = new OIC();
-             $branch = new BranchDomain($oic->findById(currentUser()->id)[0]->branch);
-             if ($_POST){
+            $branch->accept($branchReport);
+            $reportArray = $branchReport->getReportArray();
+            $offenceWithCounts=$this->FinalArrayGenerate($reportArray);
+            $heading="BRANCH REPORT----->".$branch->getBranchName();
+            $this->branchPdfReport($offenceWithCounts,$heading);
+            
+            }
+        }
+        else if (currentUser()->acl=='["HigherOfficer"]'){
+            if ($_POST){
                 $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
-                $branch->accept($branchReport);
-                $reportArray = $branchReport->getReportArray();
-             $offenceWithCounts=$this->FinalArrayGenerate($reportArray);
-             $heading="BRANCH REPORT----->".$branch->getBranchName();
-             $this->branchPdfReport($offenceWithCounts,$heading);
-                
-             }
-         }
-         else if (currentUser()->acl=='["HigherOfficer"]'){
-             if ($_POST){
-                 $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
-                 if($_POST['branch_id']<=15 && $_POST['branch_id']>=1){
-                     $branch = new BranchDomain($_POST['branch_id']); //BranchGroup
-                     $branch->accept($branchReport);
-                      $reportArray = $branchReport->getReportArray();
-             $offenceWithCounts=$this->FinalArrayGenerate($reportArray);
-             $heading="BRANCH REPORT-".$branch->getBranchName();
-             $this->branchPdfReport($offenceWithCounts,$heading);
-                 }
-             }
-         }
-         ///////from here my code
-         /*if ($_POST){
-           //This is how we can get the aray for the branch report
-            //$branch_id = 1;
-            $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
                 if($_POST['branch_id']<=15 && $_POST['branch_id']>=1){
                     $branch = new BranchDomain($_POST['branch_id']); //BranchGroup
                     $branch->accept($branchReport);
                     $reportArray = $branchReport->getReportArray();
-                    $offenceWithCounts=$this->FinalArrayGenerate($reportArray);
-                    $this->branchPdfReport($offenceWithCounts,$branch->getBranchName());
-		        } 
-         	}*/
+            $offenceWithCounts=$this->FinalArrayGenerate($reportArray);
+            $heading="BRANCH REPORT-".$branch->getBranchName();
+            $this->branchPdfReport($offenceWithCounts,$heading);
+                }
+            }
+        }
+        $this->view->render('report/branchreport');
+    }
 
-            $this->view->render('report/branchreport');
-     }
     public function overallPdfReport($offenceWithCounts,$heading){
         //$pdf = new OverallPDF();
         //$pdf->generatePDF($finesheets);
@@ -144,38 +99,6 @@ class ReportController extends Controller{
 }
 
 /*
-
-
-     public function branchreportAction(){
-         if(currentUser()->acl=='["BranchOIC"]'){
-             $oic = new OIC();
-             $branch = new BranchDomain($oic->findById(currentUser()->id)[0]->branch);
-             if ($_POST){
-                 $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
-                 $this->getReportArray($branchReport,$branch);
-             }
-         }
-         else if (currentUser()->acl=='["HigherOfficer"]'){
-             if ($_POST){
-                 $branchReport = new BranchReport($_POST['start_date'],$_POST['end_date']);
-                 if($_POST['branch_id']<=15 && $_POST['branch_id']>=1){
-                     $branch = new BranchDomain($_POST['branch_id']); //BranchGroup
-                     $this->getReportArray($branchReport,$branch);
-                 }
-             }
-         }
-         $this->view->render('report/branchreport');
-     }
-
-    public function overallPdfReport($finesheets){
-       // $pdf = new OverallPDF();
-        //$pdf->generatePDF($finesheets);
-        $overallPDF = new ConcreteAdapter1(new FPDF());//ConcreteAdapter1=OverallPDF
-        $overallPDF->setHeader(); //no branch needed
-        $overallPDF->generatePDF($finesheets);
-        $overallPDF->setFooter();
-    }
-
     public function branchPdfReport($offenceWithCounts,$branch){
        // $pdf = new BranchPDFtest();
         //$pdf->generatePDF($offenceWithCounts,$branch);
