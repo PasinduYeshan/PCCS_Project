cb4c3464244cb61df38a5c54fc9043027d39a31e
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2020-09-26 15:17:01 +0530

    .

---
+++ app/views/home/footer copy.php
@@ -0,0 +1,20 @@
+<footer class="foot">
+    <div class = "container-fluid foo" id = "service-part">
+        <div class = "row text-center">
+            <div class = "col-12">
+                <h4>Contact Us</h4>
+                <hr class = "light">
+                <div class="col-md-3">
+                    <p>0777453588</p>
+                    <p>pasinduyeshann@gmail.com</p>
+                </div>
+                
+            </div>
+
+            <div class="col-12">
+                <hr class="light-100">
+                <h5>&copy; PCCS Organization</h5>
+            </div>
+        </div>
+    </div>
+</footer>
--- app/views/home/footer.php
+++ app/views/home/footer.php
@@ -1,20 +1,86 @@
-<footer class="foot">
-    <div class = "container-fluid foo" id = "service-part">
-        <div class = "row text-center">
-            <div class = "col-12">
-                <h4>Contact Us</h4>
-                <hr class = "light">
-                <div class="col-md-3">
-                    <p>0777453588</p>
-                    <p>pasinduyeshann@gmail.com</p>
-                </div>
-                
-            </div>
 
-            <div class="col-12">
-                <hr class="light-100">
-                <h5>&copy; PCCS Organization</h5>
-            </div>
+<!-- <HEAD>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="footer.css">
+</HEAD> -->
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- <link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet"> -->
+<!--footer start from here-->
+<footer class="footer-bs">
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 col-sm-6 footer-col">
+            <div class="logofooter">Contact Us</div>
+            <p><i class="fa fa-phone"></i>Emergency: 119</p>
+            <p>
+                <i class="fa fa-phone"></i>IG's Help Desk
+                +94 11 2444480
+                +94 11 2444483
+            </p>
+            <p>Police Headquartes:</p>
+            <p>
+                <i class="fa fa-phone"></i>
+                +94 11 2421111
+            </p>
+            <p>
+                <i class="fa fa-map-marker"></i>
+                Police Headquarters, Church Street, Colombo 1
+            </p>
+
+
+        </div>
+        <div class="col-md-5 col-sm-6 footer-col">
+            <h6 class="heading7">USEFUL LINKS</h6>
+            <ul class="footer-ul">
+                <li><a href="https://www.police.lk/"> Sri Lanka Police</a></li>
+                <li><a href="https://www.navy.lk/"> Sri Lanka Navy</a></li>
+                <li><a href="https://www.army.lk/">Sri Lanka Army</a></li>
+                <li><a href="http://www.airforce.lk/"> Sri Lanka Airforce</a></li>
+
+            </ul>
+        </div>
+        <div class="col-md-2 col-sm-6 footer-col">
+            <ul class="footer-ul">
+                <li><a href="#"> Frequently Asked Questions</a></li>
+
+
+            </ul>
+            <!--<div class="post">
+                <p><a href="#"> Case Studies</a></p>
+                <p><a href="#"> Frequently Ask Questions</a></p>
+            </div>-->
+
+        </div>
+        <!--
+          <div class="col-md-3 col-sm-6 footer-col">
+          <h6 class="heading7">Social Media</h6>
+          <ul class="footer-social">
+            <li><i class="fa fa-linkedin social-icon linked-in" aria-hidden="true"></i></li>
+            <li><i class="fa fa-facebook social-icon facebook" aria-hidden="true"></i></li>
+            <li><i class="fa fa-twitter social-icon twitter" aria-hidden="true"></i></li>
+            <li><i class="fa fa-google-plus social-icon google" aria-hidden="true"></i></li>
+          </ul>
+        </div>
+            -->
+    </div>
+</div>
+</footer>
+<!--footer start from here-->
+
+<div class="copyright">
+    <div class="container">
+        <div class="col-md-7">
+            <p>© 2020 - All Rights with Sri Lanka Police</p>
+        </div>
+        <div class="col-md-5">
+            <ul class="bottom_ul">
+                <li><a href="#">Maintained by PCCS</li>
+                <li><a href="#">07111111111</a></li>
+
+            </ul>
         </div>
     </div>
-</footer>
+</div>
