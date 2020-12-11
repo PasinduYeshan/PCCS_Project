5dadbfa1804527b0a8796bef4e6891adadeee0fb
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2020-12-11 18:34:49 +0530

    some comments StripeGateway

--- app/lib/gateways/StripeGateway.php
+++ app/lib/gateways/StripeGateway.php
@@ -5,9 +5,20 @@ class StripeGateway extends AbstractGateway
 {
     public static $gateway = 'stripe';
 
+    /**
+     * return part of url needed for stripe view
+     * @return string
+     */
     public function getView(){
         return 'card_forms/stripe';
     }
+
+    /**
+     * Process the form with the post data
+     * @param $post
+     * @return array
+     * @throws \Stripe\Exception\ApiErrorException
+     */
     public function processForm($post){
         $data = [
             'amount' => $this->grandTotal * 100,
@@ -26,17 +37,33 @@ class StripeGateway extends AbstractGateway
 
 
     }
+
+    /**
+     * creates a Stripe Charge object with the data and return
+     * @param $data
+     * @return \Stripe\Charge
+     * @throws \Stripe\Exception\ApiErrorException
+     */
     public function charge($data){
         \Stripe\Stripe::setApiKey(STRIPE_PRIVATE);
         $charge = \Stripe\Charge::create($data);
         return $charge;
     }
 
+    /**
+     * Check the transaction is successful or not and get a human-readable description of the outcome type and reason
+     * @param $ch
+     */
     public function handleChargeResp($ch){
         $this->chargeSuccess = $ch->outcome->network_status == 'approved_by_network';
         $this->msgToUser = $ch->outcome->seller_message;
     }
 
+    /**
+     * Saves the transaction details in the database.
+     * @param $ch
+     * @return Transactions
+     */
     public function createTransaction($ch){
         $tx = new Transactions();
         $tx->cart_id = $this->cart_id;
