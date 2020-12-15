bb95663985f154fbc8eeb68245639f9c3438a687
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2020-12-15 19:39:12 +0530

    display validation errors in view

--- app/views/report/branchreport.php
+++ app/views/report/branchreport.php
@@ -10,13 +10,14 @@
     <h2 class="text-center">Details for branch report</h2>
     <hr>
     <form class="form" action="" method="post" target="_self">
+        <div class="bg-danger form-errors"><?=$this->displayErrors?></div>
         <?= inputBlock('date','Select start date','start_date','',['class'=>'form-control'],['class'=>'form-group col-md-6']); ?>
         <?= inputBlock('date','Select end date','end_date','',['class'=>'form-control'],['class'=>'form-group col-md-6']); ?>
         <?php
         if (currentUser()->acl=='["HigherOfficer"]') { ?>
            <div class="form-group col-md-6">
                <label for="branch">Select Branch</label>
-               <select id="branch" name="branch" class="form-control" required>
+               <select id="branch" name="branch" class="form-control">
                    <option value="" selected disabled hidden>Select branch</option>
                    <?php foreach ($this->branchlist as $branch):?>
                        <option value="<?= $branch->branch_id;?>"><?=$branch->branch_id." - ".$branch->branch_name?></option>
